95c23acf6468ea8f9b13502fe02273329c92d06d
mugambi645 <mugambi645@example.com>
2026-08-12 04:55:41 +0300

    add template backend & make accounts first database staging

--- services/api/andika/settings/base.py
+++ services/api/andika/settings/base.py
@@ -43,6 +43,22 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
 ROOT_URLCONF = "andika.urls"
 WSGI_APPLICATION = "andika.wsgi.application"
 
